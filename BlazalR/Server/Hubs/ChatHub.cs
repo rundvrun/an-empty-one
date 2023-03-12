0e5dac41ed0fb566bebd644f7f56eb55c6f99236
rundvrun <quocan.lehoang@gmail.com>
2023-03-12 19:15:09 +0700

    added missing readonly DI

--- BlazalR/Server/Hubs/ChatHub.cs
+++ BlazalR/Server/Hubs/ChatHub.cs
@@ -3,46 +3,46 @@ namespace BlazalR.Server.Hubs;
 
 public class ChatHub : Hub
 {
-    const string onPick = nameof(MessageType.OnPick);
-    const string onError = nameof(MessageType.OnError);
-    ICounter _counter;
-    public ChatHub(ICounter counter) : base()
-    {
-        _counter = counter;
-    }
+	const string onPick = nameof(MessageType.OnPick);
+	const string onError = nameof(MessageType.OnError);
+	readonly ICounter _counter;
+	public ChatHub(ICounter counter) : base()
+	{
+		_counter = counter;
+	}
 
-    public async Task PickItem(string user, string strItem)
-    {
-        var rng = new Random();
-        var count = rng.Next(3);
+	public async Task PickItem(string user, string strItem)
+	{
+		var rng = new Random();
+		var count = rng.Next(3);
 
-        if (_counter.Counter.ContainsKey(strItem))
-        {
-            if (_counter.Counter[strItem] == 0)
-            {
-                await Clients.All.SendAsync(onError, new MessageType.OnError
-                {
-                    Username = user,
-                    Message = $"{strItem} is run out"
-                });
-                return;
-            }
+		if (_counter.Counter.ContainsKey(strItem))
+		{
+			if (_counter.Counter[strItem] == 0)
+			{
+				await Clients.All.SendAsync(onError, new MessageType.OnError
+				{
+					Username = user,
+					Message = $"{strItem} is run out"
+				});
+				return;
+			}
 
-            count = int.Min(_counter.Counter[strItem], count);
-            _counter.Counter[strItem] -= count;
+			count = int.Min(_counter.Counter[strItem], count);
+			_counter.Counter[strItem] -= count;
 
-            await Clients.All.SendAsync(onPick, new MessageType.OnPick
-            {
-                Username = user,
-                ItemName = strItem,
-                Count = count
-            });
-            return;
-        }
-        await Clients.All.SendAsync(onError, new MessageType.OnError
-        {
-            Username = user,
-            Message = "Invalid item"
-        });
-    }
+			await Clients.All.SendAsync(onPick, new MessageType.OnPick
+			{
+				Username = user,
+				ItemName = strItem,
+				Count = count
+			});
+			return;
+		}
+		await Clients.All.SendAsync(onError, new MessageType.OnError
+		{
+			Username = user,
+			Message = "Invalid item"
+		});
+	}
 }
